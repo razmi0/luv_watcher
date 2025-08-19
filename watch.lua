337ecea4b03faa935ac31516555cee40b3783045
razmiO <thomas.cuesta31@gmail.com>
2025-08-20 00:27:22 +0200

    refactor: oop migration

--- watch.lua
+++ watch.lua
@@ -1,65 +1,55 @@
+#!/usr/bin/env luajit
 --
-local inspect = require("inspect")
-local uv = require("luv")
-print(inspect(uv))
---
-local handle, counter, debounce, runtime, directory_target, executed_file = nil, 0, nil, arg[1], arg[2], arg[3]
---
-if not directory_target or not executed_file then
+local uv, inspect = require("luv"), require("inspect")
+local runtime, directory, executed_file = arg[1], arg[2], arg[3]
+if not directory or not executed_file then
     error("\n\x1b[31mUsage : luajit w.lua <runtime> <watch_directory> <executed_file>\x1b[0m")
     return
 end
 
----@param path string
----@param cbs table<"on_change"|"on_error"|"on_start",fun(err? : string , filename : string)>
-local function watch(path, cbs)
-    local ev = uv.new_fs_event()
-
-    ev:start(path, { watch_entry = false },
-        function(err, filename, events)
-            if err then
-                cbs.on_error(err, filename)
-            else
-                if events.change then
-                    if debounce then uv.timer_stop(debounce) end
-                    debounce = uv.new_timer()
-                    uv.timer_start(debounce, 50, 0, function()
-                        uv.timer_stop(debounce)
-                        uv.close(debounce)
-                        debounce = nil
-                        cbs.on_change(err, filename)
-                    end)
-                end
-            end
-        end
-    )
-
-    local t = uv.new_timer()
-    uv.timer_start(t, 0, 0, function()
-        uv.timer_stop(t)
-        uv.close(t)
-        cbs.on_start(nil, path)
-    end)
+local Watch = {}
+Watch.__index = Watch
+function Watch.new(path)
+    return setmetatable({
+        events = {},
+        path = path or "./",
+        handle = nil,
+        counter = 0
+    }, Watch)
 end
---
-local function on_change(err, filename)
-    assert(not err, "\x1b[31m[Error] : Unexpected Error\x1b[0m")
+
+function Watch:on(event, cb)
+    if not event then return end
+    self.events[event] = function(err, filename)
+        if cb then cb() end
+        self:_spawn(err, filename)
+    end
+    return self
+end
+
+function Watch:_spawn(err, filename)
+    assert(not err, "\x1b[31mUnexpected Error\x1b[0m \n" .. inspect(self))
 
     local function kill()
-        if handle then
-            uv.process_kill(handle, "sigterm")
-            uv.close(handle)
-            handle = nil
+        if self.handle then
+            uv.process_kill(self.handle, "sigterm")
+            uv.close(self.handle)
+            self.handle = nil
         end
     end
 
     kill()
 
     print '\x1b[2J\x1b[H'
-    counter = counter + 1
-    print("\x1b[90m(x" .. counter .. ")\x1b[0m ")
+    self.counter = self.counter + 1
+    print(
+        "\x1b[90m" ..
+        "(x" .. self.counter .. ") " ..
+        os.date("%d/%m %H:%M") ..
+        "\x1b[0m "
+    )
 
-    handle = uv.spawn(runtime,
+    self.handle = uv.spawn(runtime,
         {
             args = { executed_file },
             stdio = { 0, 1, 2 }, -- directly pipe child stds so no wrappers possible
@@ -73,15 +63,51 @@ local function on_change(err, filename)
         end
     )
 end
+
+function Watch:run()
+    local ev = uv.new_fs_event()
+    local timer_debouncer, timer_start = nil, uv.new_timer()
+    local on_err = self.events["error"]
+    local on_change = self.events["change"]
+    local on_start = self.events["start"]
+    local function debouncer(cb, delay)
+        if timer_debouncer then uv.timer_stop(timer_debouncer) end
+        timer_debouncer = uv.new_timer()
+        uv.timer_start(timer_debouncer, delay, 0, function()
+            uv.timer_stop(timer_debouncer)
+            uv.close(timer_debouncer)
+            timer_debouncer = nil
+            cb()
+        end)
+    end
+
+    uv.timer_start(timer_start, 0, 0, function()
+        uv.timer_stop(timer_start)
+        uv.close(timer_start)
+        on_start(nil, self.path)
+    end)
+
+    ev:start(self.path, { watch_entry = false },
+        function(err, filename, events)
+            if err and on_err then
+                on_err(err, filename)
+                return
+            end
+
+            if events.change and on_change then
+                debouncer(function()
+                    on_change(err, filename)
+                end, 50)
+            end
+        end
+    )
+
+    uv.run()
+end
+
 --
-
-watch(directory_target, {
-    on_start = on_change,
-    on_change = on_change,
-    on_error = function(err, filename)
-        print("[Error] : " .. filename .. err)
-    end,
-})
-
-uv.run()
-
+Watch.new(directory)
+    :on("start", function() print("Starting..") end)
+    :on("error", function(err, filename) print("[Error] : " .. filename .. err) end)
+    :on("change")
+    :run()

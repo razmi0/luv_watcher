48b8e338dc610b0c59e5f4dbeff5b259cbe0f03a
razmiO <thomas.cuesta31@gmail.com>
2025-08-25 14:19:17 +0200

    feat: add ignore_file_list & merge prop runtime and executed file & more luadocs

--- watch.lua
+++ watch.lua
@@ -1,22 +1,43 @@
+#!/usr/bin/env luajit
 --
+
 local uv, inspect = require("luv"), require("inspect")
+
+---@class WatcherConfig
+---@field paths string[]|nil A list of directories to monitor for events such as change. Default to working directory.
+---@field exec string The command to execute when a change is detected <runtime> <executed_file>.
+---@field recursive boolean|nil Whether to monitor subdirectories recursively.
+---@field ignore string[]|nil A list of files that can be ignored.
+
+local function log_error(field)
+    error(field .. " not provided. Provide " .. field .. " = '<runtime> <executed_file>'")
+end
 
 local Watch = {}
 Watch.__index = Watch
-function Watch.new(paths, options)
+
+---@param config WatcherConfig
+function Watch.new(config)
+    if not config.exec then log_error("config.exec") end
+    local runtime, executed_file = config.exec:match("([^%s]+)%s+([^%s]+)")
+    if not runtime or not executed_file then log_error("config.exec") end
     return setmetatable({
         events = {},
-        paths = paths or { "./" },
+        paths = config.paths or { "./" },
         handle = nil,
         counter = 0,
-        recursive = options and options.recursive or false,
-        runtime = options and options.runtime or "luajit",
-        executed_file = options and options.executed_file or "main.lua"
+        recursive = config and config.recursive or false,
+        runtime = runtime,
+        executed_file = executed_file,
+        ignore_file_list = config and config.ignore or {}
     }, Watch)
 end
 
+---@param event "change"|"error"|"start"
+---@param cb function|nil
+---@return self
 function Watch:on(event, cb)
-    if not event then return end
+    if not event then return self end
     self.events[event] = function(err, filename)
         if cb then cb() end
         self:_spawn(err, filename)
@@ -100,6 +121,11 @@ function Watch:run()
 
         ev:start(self.paths[i], { recursive = self.recursive },
             function(err, filename, events)
+                for _, ignore_file in ipairs(self.ignore_file_list) do
+                    if filename == ignore_file then
+                        return
+                    end
+                end
                 if err and on_err then
                     on_err(err, filename)
                     return
@@ -116,7 +142,5 @@ function Watch:run()
 
     uv.run()
 end
-
---
 
 return Watch
